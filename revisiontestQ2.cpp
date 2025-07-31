0425e09d40a0d37532d2ff339765ee204914abd7
Ujjwaldeep Kaur <ujjwaldeepkaur@gmail.com>
2025-07-31 12:07:48 +0530

    new code

--- revisiontestQ2.cpp
+++ revisiontestQ2.cpp
@@ -1,2 +1,21 @@
-#include<iostream>
-using name space
+#include <iostream>
+using namespace std;
+int calculate(int a, int b) {
+    return a + b;
+}
+float calculate(float a, float b) {
+    return a * b;
+}
+float calculate(int a, int b, int c) {
+    return (a + b + c) / 3.0f;
+}
+int main() {
+    int sum = calculate(10, 20);                   
+    float product = calculate(3.5f, 2.0f);          
+    float average = calculate(10, 20, 30);         
+    cout << "Sum of 10 and 20 = " << sum << endl;
+    cout << "Product of 3.5 and 2.0 = " << product << endl;
+    cout << "Average of 10, 20, and 30 = " << average << endl;
+
+    return 0;
+}
